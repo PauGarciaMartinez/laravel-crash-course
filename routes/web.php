5bd8eaa0ba1b869e6feaf7e0e0814bfc97ea1738
Pau Garcia Martínez <etpau@hotmail.es>
2022-02-02 20:23:14 +0100

    Create multiple routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route that sends back a view
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
-// Route to users
+// Route to users - string
 Route::get('/users', function () {
-    return 'Welcome to the users page';
+  return 'Welcome to the users page';
+});
+
+// Route to users - Array(JSON)
+Route::get('/users/array', function() {
+  return ['PHP', 'HTML', 'Laravel'];
+});
+
+// Route to users - JSON object
+Route::get('/users', function() {
+  return response()->json([
+    'name' => 'Pau',
+    'course' => 'Laravel Crash Course'
+  ]);
+});
+
+// Route to users - function
+Route::get('/users', function() {
+  return redirect('/');
 });
 
 // firstproject.com == /
