9c0437515eed9c50e17fdc89ba92cedf809bf25b
Pau Garcia Martínez <etpau@hotmail.es>
2022-02-03 00:29:21 +0100

    Add and set images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,7 +17,7 @@
 <!-- Every div is a grid item -->
 <div class="container-grid-4">
   <div>
-      <img src="img/icon-box.jpg" alt="">
+      <img src="{{ URL('storage/icon-box.jpg') }}" alt="">
       <h2>
           Web Design
       </h2>
@@ -27,8 +27,8 @@
   </div>
 
   <div>
-      <img src="img/icon-box.jpg" alt="">
-      <h2>
+    <img src="{{ URL('storage/icon-box.jpg') }}" alt="">
+    <h2>
           Web Development
       </h2>
       <p>
@@ -37,8 +37,8 @@
   </div>
 
   <div>
-      <img src="img/icon-box.jpg" alt="">
-      <h2>
+    <img src="{{ URL('storage/icon-box.jpg') }}" alt="">
+    <h2>
           Product Design
       </h2>
       <p>
@@ -47,8 +47,8 @@
   </div>
 
   <div>
-      <img src="img/icon-box.jpg" alt="">
-      <h2>
+    <img src="{{ URL('storage/icon-box.jpg') }}" alt="">
+    <h2>
           Creative Thinker
       </h2>
       <p>
@@ -66,7 +66,7 @@
 <!-- Meet the team section -->
 <div class="container-grid-2">
   <div>
-      <img class="img-team" src="img/team-section-home.jpg" alt="">
+      <img class="img-team" src="{{ URL('storage/team-section-home.jpg') }}" alt="">
   </div>
   <div>
       <h2>
